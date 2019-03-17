91708aa93b9a4323ac919b23f3965d9f29dda26b
Philip Evans <taturuman@gmail.com>
2019-03-17 20:10:03 +0000

    Finished Commenting Account, AccountList classes

--- BackbaseApplicationProject/src/Account/Account.java
+++ BackbaseApplicationProject/src/Account/Account.java
@@ -1,44 +1,70 @@
 package Account;
-//Coding Conventions Check Bob Laramee's Coding conventions
-//Passed Rule 1 Method Length
-//Passed Rule 2 Indentation
-//Passed Rule 3 No line of Code exceeds 80 characters
-//Passed Rule 4 All class variables must start with two character sequence
-//Passed Rule 5 All class variables are accessed with accessor methods
-//Passed Rule 6 Accessor Methods come at the top
-//Passed Rule 7 Class Variables are private
-//Passed Rule 8 Method Naming public methods begin with an upper case letter
-			// 	Naming private methods with lower-case letter.
-//Passed Rule 9 Methods have no more than 5 parameters
-//Passed Rule 10 No Magic Numbers
+/**
+ * The Abstract Account class for CurrentAccount and SavingsAccount classes
+ * @author Philip Evans
+ */
 public abstract class Account 
-{
+{	
+	/**
+	 * These are class variables I deduced from the specification account
+	 * balance starts at 0. 
+	 * Assuming from Appendix1 of the brief an Account should atleast have an
+	 * AccountID(m_AccountID) and AccountType(m_AccountType). I added an extra
+	 * class variable m_AccountType to help differentiate between Current, 
+	 * Savings and Abstract accounts.
+	 * This is just an abstract class in only has the class variables that 
+	 * 
+	 * CurrentAccount and SavingsAccount share.class variables start with an 
+	 * "m_" because of the coding convention I followed.
+	 */
 	private int m_AccountID;
 	private float m_AccountBalance = 0;
 	private String m_AccountType = "ABSTRACT";
-
+	
+	/**
+	 *  Accessor/Getter for the class variable Account ID
+	 * @return the account's ID is returned
+	 */
 	public int getAccountID()
 	{
 		return m_AccountID;
 	}
 	
+	/**
+	 * Accessor/Getter for the class variable Account Balance
+	 * @return the account's balance is returned
+	 */
 	public float getAccountBalance()
 	{
 		return m_AccountBalance;
 	}
 	
+	/**
+	 * Accessor for the class variable which holds what type of Account this is
+	 * @return this current Account type of this account
+	 */
 	public String getAccountType()
 	{
 		return m_AccountType;
 	}
 	
-
+	/**
+	 * This method handles transactions
+	 * @param transactionAmount this is the float value of how much is being
+	 * added/subtracted to the current Account
+	 */
 	public void transaction(float transactionAmount)
 	{
 		this.m_AccountBalance = m_AccountBalance + transactionAmount;
 		
 	}
 	
+	/**
+	 * This method checks if the account type is valid
+	 * @param newAccountType this takes in string of raw accountType
+	 * @return this returns either "CURRENT" or "SAVINGS" and the error will be 
+	 * returned as "UNKNOWN"
+	 */
 	public static String checkType(String newAccountType) 
 	{
 		if (newAccountType.equals("CURRENT")){
@@ -54,7 +80,14 @@ public abstract class Account
 		}
 	}
 
-	
+	/**
+	 * Overloaded method of the previous method modified to take an extra param
+	 * @param newAccountType this takes in string of raw accountType
+	 * @param transactionLocation location of the transaction in the ledger/
+	 * csv file
+	 * @return this returns either "CURRENT" or "SAVINGS" and the error will be 
+	 * returned as "UNKNOWN"
+	 */
 	public static String checkType(String newAccountType,int transactionLocation
 	) 
 	{
--- BackbaseApplicationProject/src/Account/AccountLists.java
+++ BackbaseApplicationProject/src/Account/AccountLists.java
@@ -1,25 +1,32 @@
 package Account;
-//Coding Conventions Check Bob Laramee's Coding conventions
-//Passed Rule 1 Method Length
-//Passed Rule 2 Indentation
-//Passed Rule 3 No line of Code exceeds 80 characters
-//Passed Rule 4 All class variables must start with two character sequence
-//Passed Rule 5 All class variables are accessed with accessor methods
-//Passed Rule 6 Accessor Methods come at the top
-//Passed Rule 7 Class Variables are private
-//Passed Rule 8 Method Naming public methods begin with an upper case letter
-			// 	Naming private methods with lower-case letter.
-//Passed Rule 9 Methods have no more than 5 parameters
-//Passed Rule 10 No Magic Numbers
 import java.util.ArrayList;
-
 import Transactions.Transaction;
-
+/**
+ * This is the AccountList class which makes it easier to return multiple 
+ * the ArrayList of Accounts and ArrayList of AccountID in integer this is
+ * useful for searching for Accounts
+ * @author Philip Evans
+ *
+ */
 public class AccountLists 
 {
+/**
+ * AccountList and AccountIDChecker is an Array mainly because they are flexible
+ * and it easy to manipulate. I have used these extensively in university
+ * projects
+ * class variables start with an "m_" because of the coding convention I 
+ * followed.
+ */
 private ArrayList<Account> m_AccountList = new ArrayList<Account>();
 private ArrayList<Integer> m_AccountIDChecker = new ArrayList<Integer>();
-
+	
+	/**
+	 * This is the first constructor of the AccountList Class
+	 * @param accountList this is an ArrayList of accounts recovered from the 
+	 * input file
+	 * @param accountIDChecker this the ID of Accounts available used for 
+	 * validation
+	 */
 	public AccountLists(ArrayList<Account> accountList, ArrayList<Integer> 
 	accountIDChecker)
 	{
@@ -27,6 +34,29 @@ private ArrayList<Integer> m_AccountIDChecker = new ArrayList<Integer>();
 		this.m_AccountIDChecker = accountIDChecker;
 	}
 	
+	/**
+	 * This is an overloaded method for the AccountList Constructor
+	 * 
+	 * While loop checks every transaction object in the ledger.
+	 * Inside the while loop is an if statement which checks if the accountID of
+	 * the transaction object exists in the accountIDchecklist to check if it is
+	 * already added to the ArrayList of Accounts accountList. If it doesn't
+	 * exist then the nested statements are triggered.
+	 * 
+	 * Nested inside the first if statement another if, else if and else
+	 * statements are found. If these statements are creates the correct account
+	 * and adds it to the accountList and the accoundIDCheckList if the else
+	 * statement is triggered it returns an error.
+	 * 
+	 * @param ledger the ArrayList of Transactions generated from the input csv 
+	 * file
+	 * @param accountIDCheckList this the ID of Accounts available used for 
+	 * validation the m_AccountIDChecker of the current object will become the
+	 * same as this param at the end of this methods execution.
+	 * @param accountList ArrayList of accounts generated from the input csv 
+	 * file m_AccountList of the current object will become the
+	 * same as this param at the end of this methods execution.
+	 */
 	public AccountLists(ArrayList<Transaction> ledger, ArrayList<Integer> 
 	accountIDCheckList, ArrayList<Account> accountList)
 	{
@@ -65,27 +95,41 @@ private ArrayList<Integer> m_AccountIDChecker = new ArrayList<Integer>();
 		this.m_AccountList = accountList;
 		this.m_AccountIDChecker = accountIDCheckList;
 	}
-	
+
+	/**
+	 * Accessor/Getter for the class variable m_AccountList
+	 * @return List of the current customers accounts
+	 */
 	public ArrayList<Account> getAccountList() 
 	{
 		return m_AccountList;
 	}
 	
-	public void setAccountList(ArrayList<Account> accountList) 
-	{
-		this.m_AccountList = accountList;
-	}
-	
+	/**
+	 * Accessor/Getter for the class variable m_AccountIDChecker
+	 * @return list of the current customer accountIDs
+	 */
 	public ArrayList<Integer> getAccountIDChecker() 
 	{
 		return m_AccountIDChecker;
 	}
 	
-	public void setAccountIDChecker(ArrayList<Integer> accountIDChecker) 
-	{
-		this.m_AccountIDChecker = accountIDChecker;
-	}
-	
+	/**
+	 * This method differentiates which account in the list is the customers 
+	 * Current Account or Savings Account. The While goes through the entire
+	 * accountList(which should be a size of 2) and then the if statement uses
+	 * the boolean values of svaingsAccountFound and currentAccountFound. If 
+	 * either of these values are false then it will execute the nest if and
+	 * else. The nested if catches the current account being evaluated is a 
+	 * Savings account, if it is then saves the location in accountList as an
+	 * int savingsAccountLocation. The else statement does the same but for 
+	 * Current accounts. Then finally the else statement just increases the
+	 * count to keep the search going.
+	 * 
+	 * @param accountList an ArrayList of accounts
+	 * @return overallResult contains the location of the savingsAccount and
+	 * currentAccount in the accountList as String seperated by a comma
+	 */
 	public static String findCustomerAccounts(ArrayList<Account> accountList)
 	{
 		int savingsAccountLocation = 0;
@@ -97,24 +141,29 @@ private ArrayList<Integer> m_AccountIDChecker = new ArrayList<Integer>();
 			String accountType = Account.checkType(accountList.get(count).
 			getAccountType());
 			if(savingsAccountFound == false || currentAccountFound == false){
-			if (accountType.equals("SAVINGS") && savingsAccountFound == false){
-				savingsAccountLocation = count;
-				savingsAccountFound = true;
+				if(accountType.equals("SAVINGS")&&savingsAccountFound == false){
+					savingsAccountLocation = count;
+					savingsAccountFound = true;
+					count++;
+				}else if(accountType.equals("CURRENT") && currentAccountFound == 
+				false){
+					currentAccountLocation = count;	
+					currentAccountFound = true;
+					count++;
+				}else{
 				count++;
-			}else if(accountType.equals("CURRENT") && currentAccountFound == 
-			false){
-				currentAccountLocation = count;	
-				currentAccountFound = true;
-				count++;
-			}else{
-			count++;
-			}		
-		}
+				}		
+			}
 		}		
 		String overallResult=savingsAccountLocation+","+currentAccountLocation;
 		return overallResult;
 	}
 	
+	
+	/**
+	 * Method that prints out an ArrayList filled with accounts;
+	 * @param accountList
+	 */
 	public static void printAccountArrayList(ArrayList<Account> accountList)
 	{
 		int count = 0;
